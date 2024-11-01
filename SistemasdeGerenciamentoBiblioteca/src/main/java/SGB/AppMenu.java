b61c4caa8fec67f7f416fdf446108b5681078619
Tiago Cardoso Ferreira <tiagocardoso1357@gmail.com>
2024-11-01 10:55:03 -0300

    criação da estrutura do menu

--- SistemasdeGerenciamentoBiblioteca/src/main/java/SGB/AppMenu.java
+++ SistemasdeGerenciamentoBiblioteca/src/main/java/SGB/AppMenu.java
@@ -1,9 +1,13 @@
 package SGB;
 
+import java.util.Scanner;
+
 public class AppMenu {
     public static void main(String[] args) {
-        System.out.println("--------Sistema de Gerenciamento de Biblioteca---------");
-        System.out.println("Selecione uma opção: ");
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("||||||||||Sistema de Gerenciamento de Biblioteca||||||||||");
+        System.out.println("Opções disponíveis: ");
         System.out.println("1 - Cadastrar Livro");
         System.out.println("2 - Cadastrar Usuário");
         System.out.println("3 - Realizar Empréstimo");
@@ -12,5 +16,40 @@ public class AppMenu {
         System.out.println("6 - Listar Usuários");
         System.out.println("7 - Verificar Empréstimos Ativos");
         System.out.println("8 - Sair");
+        
+        System.out.println("Qual opção você deseja? ");
+        int opcMenu = scan.nextInt();
+        
+        switch (opcMenu){
+            case 1:
+                //....
+                break;
+            case 2:
+                //....
+                break;
+            case 3:
+                //....
+                break;
+            case 4:
+                //....
+                break;
+            case 5:
+                //....
+                break;
+            case 6:
+                //....
+                break;
+            case 7:
+                //....
+                break;
+            case 8:
+                //....
+                break;
+            default:
+                //....
+                break;
+        }
+        
+        
     }    
 }
