4bb04f26c7f23e94dd4808d4aa996f87439dbf99
Charlles <carvalhocharlles4@gmail.com>
2024-11-02 18:23:57 -0300

    Criação da classe Livros e determinando atributos

---
+++ SistemasdeGerenciamentoBiblioteca/src/main/java/SGB/Livros.java
@@ -0,0 +1,11 @@
+package SGB;
+
+public class Livros {
+  private int id_livro;
+  private String titulo;
+  private String autor;
+  private String editora;
+  private int ano_publi;
+  private int num_exemplares;
+
+}
