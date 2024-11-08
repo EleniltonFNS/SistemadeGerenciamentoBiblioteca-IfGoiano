825349ad4aa3a4901a58b12432ad381a8db6eebb
Charllesneto <carvalhocharlles4@gmail.com>
2024-11-08 13:52:22 -0300

    comentarios - Livros.java

--- SistemasdeGerenciamentoBiblioteca/src/main/java/SGB/Livros.java
+++ SistemasdeGerenciamentoBiblioteca/src/main/java/SGB/Livros.java
@@ -9,9 +9,9 @@ public class Livros {
   private int num_exemplares;
   private int num_exemp_disp;
   
-  // Todo livro tem que ter id, titulo, autor, editora, ano que foi publicado e numero de exemplares;
+  // Constructor: livro tem que ter id, titulo, autor, editora, ano que foi publicado e numero de exemplares;
   public Livros(int id_livro, String titulo, String autor, String editora, int ano_publi, int num_exemplares) {
-    try {
+    try { // Tratar erro caso o construtor de erro;
       this.id_livro = id_livro;
       this.titulo = titulo;
       this.autor = autor;
@@ -24,31 +24,33 @@ public class Livros {
     }
   }
 
-  public int getId_livro() {
+  public int getId_livro() { // Ver ID livro;
     return id_livro;
   }
 
-  public String get_Titulo(){ //Ver titulo
+  public String get_Titulo(){ // Ver titulo;
     return titulo;
   }
 
-  public int get_num_exemplares(){ //Ver numero de exemplares
+  public int get_num_exemplares(){ // Ver numero de exemplares;
     return num_exemplares;
   }
 
-  public int get_num_exemp_disp(){ //Ver numero de exemplares
+  public int get_num_exemp_disp(){ // Ver numero de exemplares;
     return num_exemp_disp;
   }
-  public void atualizarNumExemp(int valor){
+  public void atualizarNumExemp(int valor){ // Altera a quantidade de livros disponíveis caso seja emprestado -1 caso seja devolução +1;
     this.num_exemp_disp +=valor;
   }
 
   @Override
-  public String toString(){ //Utilizar para imprimir os livros.
-    if(num_exemp_disp == num_exemplares) {
+  public String toString(){ // Imprimi os livros;
+    
+    if(num_exemp_disp == num_exemplares) { // Se número de exemplares e exemplares disponíveis for igual simplifica a para não ter dois números repetidos;
       return " | ID: " + id_livro + "\n | Título: " + titulo + "\n | Autor: " + autor + "\n | Editora: " + editora +
-              "\n | Ano de Publicação: " + ano_publi + "\n | Números de Exemplares: " + num_exemplares;
-    } else {
+              "\n | Ano de Publicação: " + ano_publi + "\n | Números de Exemplares: " + num_exemplares + " - Todos os exmplares disponíveis. ";
+      
+    } else { // Se tiver algum livro emprestado vai imprimir número de exemplares e exemplares disponíveis separadamente;
       return " | ID: " + id_livro + "\n | Título: " + titulo + "\n | Autor: " + autor + "\n | Editora: " + editora + "\n | Ano de Publicação: "
               + ano_publi + "\n | Números de Exemplares: " + num_exemplares + "\n | Exemplares Disponíveis: " + num_exemp_disp;
     }
