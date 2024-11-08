d9085a87095f4cc6f01fea0f07536c9294fe98a5
Luanna-f <146787119+Luanna-f@users.noreply.github.com>
2024-11-08 13:21:54 -0300

    Definição dos comentários da classe Clientes.java

--- SistemasdeGerenciamentoBiblioteca/src/main/java/SGB/Clientes.java
+++ SistemasdeGerenciamentoBiblioteca/src/main/java/SGB/Clientes.java
@@ -1,6 +1,6 @@
 package SGB;
 
-public class Clientes {
+public class Clientes { // Atributos, declaração das variaveis que irá receber os dados principais
     private int idCliente;
     private String nome;
     private boolean temEmprestimo;
@@ -9,6 +9,7 @@ public class Clientes {
     private String telefone;
     private Livros livroEmprestado;
     
+    // Método construtor que inicializa um novo cliente com os dados essenciais e já marcando como sem empréstimo
     public Clientes (int idCliente, String nome, String email,String  cpf, String telefone) {
         try {
             this.idCliente = idCliente;
@@ -23,30 +24,31 @@ public class Clientes {
         }
     }
     
-    public String getNome() {
+    public String getNome() { // Retorna o nome do cliente
         return nome;
     }
-    public int getIdCliente() {
+    public int getIdCliente() { // Retorna o ID do cliente
         return idCliente;
     }
-    public boolean getTemEmprestimo() { //Verifica se "temEmprestimo" é verdadeiro ou falso
+    public boolean getTemEmprestimo() { // Retorna "temEmprestimo", se for verdadeiro (usuário tem empréstimo), se for falso (usuário não tem empréstimo)
         return temEmprestimo;
     }
-    public void emprestimo_true(Livros livro){ //Marca que o cliente está com algum livro emprestado
+    public void emprestimo_true(Livros livro){ // Marca que o cliente está com algum livro emprestado, associando o livro ao cliente
         temEmprestimo = true;
         this.livroEmprestado = livro;
     }
-    public void emprestimo_false(){ //Marca que o cliente não está com nenhum livro
+    public void emprestimo_false(){ // Marca que o cliente não está com nenhum livro, liberando o livro ao cadastro do cliente
         temEmprestimo = false;
         this.livroEmprestado = null;
     }
 
     @Override
     public String toString (){
-        if(!temEmprestimo) {
+        if(!temEmprestimo) { // Retorna os dados do cliente, caso ele não tenha algum empréstimo em seu nome
             return " | ID Cliente: " + idCliente + "\n | Nome: " + nome + "\n | Email: " + email + "\n | CPF: " + cpf +
                     "\n | Telefone: " + telefone + "\n | Não possui empréstimo ativo.";
-        } else {
+            
+        } else { // Retorna os dados do cliente junto ao titulo e Id do livro, caso ele tenha algum empréstimo em seu nome
             return " | ID Cliente: " + idCliente + "\n | Nome: " + nome + "\n | Email: " + email + "\n | CPF: " + cpf +
                     "\n | Telefone: " + telefone + "\n --------------------------------" + "\n | Empréstimo ativo:" +
                     "\n | Título: " + livroEmprestado.get_Titulo() + " - Id Livro: " + livroEmprestado.getId_livro();
